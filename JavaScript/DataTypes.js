eda5690da842051696ff93bb3378b211d84eb274
AKSHRA_0304 <153852401+Akshra-123@users.noreply.github.com>
2024-08-26 14:25:18 +0000

    DataTypes in JS

---
+++ JavaScript/DataTypes.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all the js code as a newer version
+
+// alert(2+4) 
+// we are working in node so this statement is giving error but if we write this same statement in browser an alert box will be opened
+
+/* difference between null and undefined 
+undefined toh mtlb intentionally blank hai 
+and null means ki koi value hai hi ni */
+
+/* A variable that has been declared but not yet assigned a value is undefined
+null is an assignment value that explicitly indicates the absence of any object value. */
+
+/* for uniqueness symbol datatype is used */
+
+console.log(typeof null)  // it is of type object
+console.log(typeof undefined) // it is of type undefined which means it has a separate type
