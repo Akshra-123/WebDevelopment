4bff05297e4484ae5f46d27a6ccf4544d2cb9543
AKSHRA_0304 <153852401+Akshra-123@users.noreply.github.com>
2024-08-27 12:57:43 +0000

    StringOperations

---
+++ JavaScript/StringOperations.js
@@ -0,0 +1,12 @@
+console.log(1+2)
+console.log("1"+"2")
+console.log("1"+2)
+console.log(1+"2")
+console.log("1" + 2 +2)
+console.log(1+2+"2")
+console.log(+true)
+
+// prefix and postfix operator
+let a = 12
+console.log(a++)
+console.log(+a)
