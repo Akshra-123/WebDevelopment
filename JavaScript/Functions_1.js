d129b5b90c97f9980b8ee19d542356e138bf9192
AKSHRA_0304 <153852401+Akshra-123@users.noreply.github.com>
2024-12-19 12:12:19 +0000

    Introduction to functions

---
+++ JavaScript/Functions_1.js
@@ -0,0 +1,15 @@
+// Name Function
+function MyName() {
+    console.log("Akshra")
+}
+
+MyName()
+
+// Addition Function
+function addition(number1 , number2) {
+    console.log(number1+number2)
+}
+addition(12,23)
+
+// when the function is defined toh uss time jo values hoti hai unko parameters bolte hai and jab function call krte hai uss time
+// jo values hoti hai those are arguments
