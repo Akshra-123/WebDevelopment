a0a62d5b1b4bf99e001067c60352fa259f120adb
AKSHRA_0304 <153852401+Akshra-123@users.noreply.github.com>
2024-12-25 18:03:56 +0000

    nested scope

--- JavaScript/Scope/NestedScope.js
+++ JavaScript/Scope/NestedScope.js
@@ -5,6 +5,7 @@ function one() {
         const website = "OpenAI"
         console.log(username)
     }
+    console.log(website) // this part will not work as the scope of nested function i.e two is not defined outside 
     two()
 }
 one()
