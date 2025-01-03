edf4f1b212232b2a4e206103efebbf2da4c0aa82
AKSHRA_0304 <153852401+Akshra-123@users.noreply.github.com>
2025-01-03 18:44:43 +0000

    Arrow Function

--- JavaScript/Functions/ThisAndArrowFunction.js
+++ JavaScript/Functions/ThisAndArrowFunction.js
@@ -11,4 +11,24 @@ const user = {
 
 console.log(user.welcome_message())
 
+// arrow function
 
+// syntax of arrow function
+// explicit return i.e when return keyword is used
+const arrow_func = (num1 , num2) => {
+    return num1 + num2;
+}
+console.log(arrow_func(2,3))
+
+// implicit return i.e when return keyword is not used 
+const add_two = (num1 , num2) => num1 + num2
+
+console.log(add_two(3,4))
+
+/* curly braces use honge toh return keyword use hoga but if I use directly without any parenthesis or if I use parenthesis 
+then there is no need of return keyword */
+
+// the main use of parenthesis is when we have to return objects 
+
+const name = () => ({username:"Alia"})
+console.log(name()) 
